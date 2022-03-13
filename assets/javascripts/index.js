33be75d84795390dbdcfe5c9656a63b1d855c2ea
cwkim5005 <cwkim5005@gmail.com>
2022-03-14 05:55:42 +0900

    123

---
+++ assets/javascripts/index.js
@@ -0,0 +1,23 @@
+var progress_bar = 0
+function getAnswer(){
+  if(password.answer.value == "문제 1번"){
+    location.href="scripts1/h1.html"
+  }else if (password.answer.value == "문제 2번") {
+    location.href="scripts1/h2.html"
+  }else if (password.answer.value == "문제 3번") {
+    location.href="scripts1/h3.html"
+  }else if (password.answer.value == "문제 4번") {
+    location.href="scripts1/h4.html"
+  }else if (password.answer.value == "문제 5번") {
+    location.href="scripts1/h5.html"
+  }else if (password.answer.value == "문제 6번") {
+    location.href="scripts1/h6.html"
+  }else if (password.answer.value == "문제 7번") {
+    location.href="scripts1/h7.html"
+  }else{
+    alert("비밀번호가 틀렸습니다.")
+  }
+}
+function progress() {
+  document.getElementById('pb').value = '20';
+}
---
+++ assets/javascripts/questions.js
@@ -0,0 +1,22 @@
+function getAnswer(i){
+  if(quiz.answer.value == i){
+    alert("정답입니다.");
+    if(i == 12){ //문제 1번
+      location.href="../scripts2/t1.html"
+    }else if(i == 59372418){ //문제 2번
+      location.href="../scripts2/t2.html"
+    }else if(i == 13){ //문제 3번
+      location.href="../scripts2/t3.html"
+    }
+    else if(i == 33){ //문제 4번
+      location.href="../scripts2/t4.html"
+    }
+    else if(i == "나는 박사의 부활을 바란다"){ //문제 4번
+      location.href="../scripts2/t5.html"
+    }else if(i == "KA8J"){ //문제 4번
+      location.href="../scripts2/t6.html"
+    }
+  }else{
+    alert("틀렸습니다.")
+  }
+}
